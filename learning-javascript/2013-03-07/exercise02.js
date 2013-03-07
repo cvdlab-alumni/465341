513e50904a0bfd6870db769d9081bebf898a29e6
robtesta87 <roberto.testa87@gmail.com>
2013-03-07 15:16:17 +0100

    exercise02 completato

--- learning-javascript/2013-03-07/exercise02.js
+++ learning-javascript/2013-03-07/exercise02.js
@@ -1,8 +1,8 @@
 function fibonacci(i){
-if(i===0)
-return 0;
-else if(i===1 || i===2)
-return 1;
-else
-return fibonacci(i-1)+fibonacci(i-2);
+	if(i===0)
+		return 0;
+	else if(i===1 || i===2)
+			return 1;
+		else
+			return fibonacci(i-1)+fibonacci(i-2);
 }
